d883bd31760b7d595dd16401303e217a06f7aba2
BanisBT <banisbt@gmail.com>
2022-01-10 14:59:09 +0200

    first commit /

--- src/main/kotlin/com/tbarauskas/Application.kt
+++ src/main/kotlin/com/tbarauskas/Application.kt
@@ -3,12 +3,16 @@ package com.tbarauskas
 import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
 import com.tbarauskas.plugins.*
+import io.ktor.application.*
+
+fun Application.configure() {
+    configureRouting()
+    configureMonitoring()
+    configureTemplating()
+    configureSerialization()
+}
 
 fun main() {
     embeddedServer(Jetty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureMonitoring()
-        configureTemplating()
-        configureSerialization()
     }.start(wait = true)
 }
